3856676e29f36b0787eae8468537f4df0a70733c
Natalie Frick <54511498+nataliefrick@users.noreply.github.com>
2022-10-20 12:09:08 +0200

    searchfunction

--- app/Http/Controllers/PlantController.php
+++ app/Http/Controllers/PlantController.php
@@ -23,22 +23,7 @@ class PlantController extends Controller
         // return Plant
         // $plant = Plant::where('title', 'like',  '%' . $searchTerm . '%')->get(); //works
         $plant = Plant::where(DB::raw('LOWER(title)'), 'like',  '%' . strtolower($searchTerm) . '%')->orWhere(DB::raw('LOWER(description)'), 'like',  '%' . strtolower($searchTerm) . '%')->get();
-        // $plant = Plant::where(DB::raw('lower(title)'), strtolower($searchTerm))->get();
-        
-        // use DB;
-        // $plant = Plant::whereRaw(DB::raw('lower(`title`)'), 'like',  '%' . $searchTerm . '%')->get(); 
-        // $plant = Plant::whereRaw("title COLLATE UTF8_GENERAL_CI LIKE %{$searchTerm}%")->get();
-        // strtolower($searchTerm)
-        // $search = mb_strtolower(trim(request()->input('search')));
-        // $plant = Plant::where('LOWER(`title`)', 'like',  '%' . $searchTerm . '%')->get();
-        // ->toArray();
-
-        // User::query()
-        // ->where('name', 'LIKE', "%{$searchTerm}%")
-        // ->orWhere('email', 'LIKE', "%{$searchTerm}%")
-        // ->get();
-
-        
+                
         if ($plant == null) {
             return response()->json([
                 'No item matching this search term was found.'
