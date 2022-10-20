3cb72625eb323c32c1c27f1c7b24f61a9b82d28b
Natalie Frick <54511498+nataliefrick@users.noreply.github.com>
2022-10-20 12:02:46 +0200

    searchfunction

--- app/Http/Controllers/PlantController.php
+++ app/Http/Controllers/PlantController.php
@@ -22,7 +22,7 @@ class PlantController extends Controller
     public function searchTitle($searchTerm) {
         // return Plant
         // $plant = Plant::where('title', 'like',  '%' . $searchTerm . '%')->get(); //works
-        $plant = Plant::where(DB::raw('LOWER(title)'), 'like',  '%' . $searchTerm. '%')->get();
+        $plant = Plant::where(DB::raw('LOWER(title)'), 'like',  '%' . strtolower($searchTerm) . '%')->get();
         // $plant = Plant::where(DB::raw('lower(title)'), strtolower($searchTerm))->get();
         
         // use DB;
