f2b824706c519bc145c67943b0f1c2b608a24ae8
Natalie Frick <54511498+nataliefrick@users.noreply.github.com>
2022-10-25 10:14:49 +0200

    table 2

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employee;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Employee::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // --validate incoming data
+        $request->validate([
+            'name'=> 'required',
+            'email'=> 'required',
+            'password'=> 'required'
+        ]);
+
+        return Employee::create($request->all());
+    
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Employee $employee)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Employee $employee)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // return $plant->delete();
+        $employee = Employee::find($id);
+
+        if($employee != null){
+            $employee->delete($id);
+            return response()->json(['Employee deleted.']);
+        } else {
+            return response()->json(['Employee not found.'], 404);
+        }
+    }
+
+}
